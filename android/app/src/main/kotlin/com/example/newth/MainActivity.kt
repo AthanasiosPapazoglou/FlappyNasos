3b4da855db117b606d411a567e1e7da0b500813f
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-09-22 21:28:49 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/newth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  double birdY = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.blue,
+              child: Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      alignment: Alignment(0, birdY),
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color:Colors.yellow,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.brown[400],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
